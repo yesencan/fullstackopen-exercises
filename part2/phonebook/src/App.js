ebb7d3d73e625a2bb90f607d041eb393c236e2d0
Yunus Emre Şencan <yunusemresencan0@gmail.com>
2022-07-19 19:30:33 +0300

    Add deployment compatibility

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import Form from './components/Form'
 import numberService from './services/numbers'
 import Persons from './components/Persons'
